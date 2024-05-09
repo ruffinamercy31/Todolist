75d8e3b95955b158e4e85f3f166e05b80a4485a7
ruffinamercy31 <ruffina.m@edufin.in>
2024-05-09 15:47:13 +0530

    removed unsed components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import "./App.css";
-import AddForm from "./app/components/AddForm";
 import Todo from "./app/components/Todo";
-import SignupForm from "./app/components/SignUp";
 
 function App() {
   const handleActionChange = () => {};
   return (
     <div className="">
-      {/* <AddForm /> */}
       <Todo />
-      {/* <SignupForm handleActionChange={handleActionChange} /> */}
     </div>
   );
 }
